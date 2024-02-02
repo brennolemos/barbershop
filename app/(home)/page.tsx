199a9c31b7b8f4df8a5ce6dd4c8b75ac4cf5758b
Breno Lemos <brenomiros@gmail.com>
2024-02-02 19:07:20 -0300

    feat: add search component view

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,22 @@
-import Image from "next/image";
-import Header from "../_components/header";
+import Header from "@/app/_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-      <div className="px-5 pt-5">
+      <div className="p-6">
         <h2 className="text-xl font-bold ">Olá, Breno</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
         </p>
+      </div>
+
+      <div className="px-5">
+        <Search />
       </div>
     </div>
   );
