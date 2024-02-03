5d0a7d8472ebfd41ff9921e02b4f9ee0ac9529a1
Breno Lemos <brenomiros@gmail.com>
2024-02-03 16:08:02 -0300

    feat: add rating badge

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,6 +1,8 @@
+import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
 
 type BarbershopItemProps = {
@@ -12,6 +14,16 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="p-0 px-1">
         <div className=" relative w-full h-[160px]">
+          <div className="absolute top-2 left-2 z-50">
+            <Badge
+              variant="secondary"
+              className="opacity-85 flex items-center gap-1"
+            >
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
+            </Badge>
+          </div>
+
           <Image
             fill
             sizes="100vw"
