5709ade1480797cf6a11cd5520c05e78ff9b7687
Breno Lemos <brenomiros@gmail.com>
2024-02-04 16:28:10 -0300

    feat: add the BarbershopDetailsPage header

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
 
 type BarbershopDetailsPageProps = {
   params: {
@@ -23,7 +26,46 @@ const BarbershopDetailsPage = async ({
     return null;
   }
 
-  return <div>{barbershop.name}</div>;
+  return (
+    <div>
+      <div className="h-[250px] w-full relative">
+        <Button
+          size="icon"
+          variant="outline"
+          className="absolute z-50 top-4 left-4"
+        >
+          <ChevronLeftIcon />
+        </Button>
+
+        <Button
+          size="icon"
+          variant="outline"
+          className="absolute z-50 top-4 right-4"
+        >
+          <MenuIcon />
+        </Button>
+
+        <Image
+          src={barbershop.imageUrl}
+          fill
+          className="object-cover opacity-75"
+          alt={barbershop.name}
+        />
+      </div>
+
+      <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
+        <h1 className="text-xl font-bold ">{barbershop.name}</h1>
+        <div className="flex items-center gap-1 mt-2">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop.address}</p>
+        </div>
+        <div className="flex items-center gap-1 mt-2">
+          <StarIcon className="text-primary" size={18} />
+          <p className="text-sm">5,0 (900 avaliações)</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BarbershopDetailsPage;
