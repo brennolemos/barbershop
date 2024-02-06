5cc581c0d6e89bbbfe09f643e64b8edf8b6e8bc7
Breno Lemos <brenomiros@gmail.com>
2024-02-06 16:41:26 -0300

    feat: add Sidemenu component

---
+++ app/_components/sidemenu.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { CalendarIcon, HomeIcon, LogOutIcon, UserIcon } from "lucide-react";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { SheetHeader, SheetTitle } from "./ui/sheet";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Sidemenu = () => {
+  const { data } = useSession();
+
+  const handleLogoutClick = () => signOut();
+
+  const handleLoginClick = async () => {
+    await signIn("google");
+  };
+
+  return (
+    <>
+      <SheetHeader className="border-b border-solid border-secondary p-5">
+        <SheetTitle>Menu</SheetTitle>
+      </SheetHeader>
+
+      {data?.user ? (
+        <div className="flex justify-between items-center px-5 py-6">
+          <div className="flex items-center gap-3 ">
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ""} />
+            </Avatar>
+
+            <h2 className="font-bold">{data.user.name}</h2>
+          </div>
+
+          <Button variant="secondary" size="icon" onClick={handleLogoutClick}>
+            <LogOutIcon />
+          </Button>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-3 px-5 py-6">
+          <div className="flex items-center gap-3">
+            <UserIcon />
+            <h2 className="font-bold">Olá, faça seu login</h2>
+          </div>
+
+          <Button
+            variant="secondary"
+            className="w-full"
+            onClick={handleLoginClick}
+          >
+            Fazer Login
+          </Button>
+        </div>
+      )}
+
+      <div className="flex flex-col gap-3 px-5">
+        <Button variant="outline" className="justify-start" asChild>
+          <Link href="/">
+            <HomeIcon size={18} className="mr-2" />
+            Início
+          </Link>
+        </Button>
+
+        {data?.user && (
+          <Button variant="outline" className="justify-start" asChild>
+            <Link href="/bookings">
+              <CalendarIcon size={18} className="mr-2" />
+              Agendamentos
+            </Link>
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Sidemenu;
