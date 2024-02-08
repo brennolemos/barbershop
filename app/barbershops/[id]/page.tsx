aed43f457f3d38f52e3cc626e521ed6ad1b0ef39
Breno Lemos <brenomiros@gmail.com>
2024-02-08 16:34:32 -0300

    fix: remove typos in prisma schema

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -37,7 +37,7 @@ const BarbershopDetailsPage = async ({
       <BarbershopInfo barbershop={barbershop} />
 
       <div className="px-5 flex flex-col gap-4 py-6">
-        {barbershop.Service.map((service) => (
+        {barbershop.services.map((service) => (
           <ServiceItem
             key={service.id}
             service={service}
